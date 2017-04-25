6b4062de3c96a3654ff7ac053931309b38d2b587
Michael Krieg <krieg@centrias-colocation.de>
2017-04-25 13:28:13 +0200

    streamline the download and install process a bit

---
+++ README.md
@@ -0,0 +1,4 @@
+Getting started
+===============
+
+curl -s -L https://raw.githubusercontent.com/centriascolocation/lynis/1.1/get-lynis-latest.sh | bash
--- get-lynis-latest.sh
+++ get-lynis-latest.sh
@@ -2,6 +2,20 @@
 
 VERSION=2.4.8
 
+function echo_exit()
+{
+    echo "$*"
+    exit 1
+}
+
+wget --version >/dev/null || echo_exit "wget command not found. Please install first."
+
 wget --no-clobber -q -O lynis-${VERSION}.tar.gz  https://github.com/CISOfy/lynis/archive/${VERSION}.tar.gz
 
+tar xfz lynis-${VERSION}.tar.gz
 
+(
+  cd lynis-${VERSION}
+  ./lynis show version ; ./lynis update info
+  echo "Lynis is ready to use. Please find it here: $(pwd)"
+)
