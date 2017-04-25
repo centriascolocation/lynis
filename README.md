6fad09f76df94f4a152d3232fc458a151fc87e18
Michael Krieg <krieg@centrias-colocation.de>
2017-04-25 15:20:32 +0200

    integrity check and verification before using the software

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 Getting started
 ===============
 
-curl -s -L https://raw.githubusercontent.com/centriascolocation/lynis/1.1/get-lynis-latest.sh | bash
+Download and extract to a local directory
+-----------------------------------------
+
+```
+curl -s -L https://raw.githubusercontent.com/centriascolocation/lynis/1.2/get-lynis-latest.sh | bash
+```
+
+The first tests
+---------------
+
+Assume you've successfully downloaded version 2.4.8:
+
+```
+cd lynis-2.4.8
+./lynis audit system --auditor "Michael Krieg <michael@centrias.de>" --pentest
+```
+
+Hint: you could omit the "--pentest" option for a full privileged (root/sudo) audit!
+
--- get-lynis-latest.sh
+++ get-lynis-latest.sh
@@ -1,6 +1,8 @@
 #/usr/bin/env sh
 
 VERSION=2.4.8
+SHA1_HASH="91b7e94c1b39c4b41340c55c0ff601e04aef7387"
+SHA256_HAHS="6be0dcb0e3c66c76266944b1c5b6f1d8ba617dc1ce89b8d278f4e1f990a6f70a"
 
 function echo_exit()
 {
@@ -8,14 +10,29 @@ function echo_exit()
     exit 1
 }
 
+echo "checking wget ..."
 wget --version >/dev/null || echo_exit "wget command not found. Please install first."
 
-wget --no-clobber -q -O lynis-${VERSION}.tar.gz  https://github.com/CISOfy/lynis/archive/${VERSION}.tar.gz
+echo "downloading lynis in version: ${VERSION} ..."
+wget --no-clobber -q -O lynis-${VERSION}.tar.gz https://cisofy.com/files/lynis-${VERSION}.tar.gz
 
-tar xfz lynis-${VERSION}.tar.gz
+echo "checking downloaded file integrity ..."
+if which openssl >/dev/null; then
+    _SHA1=$(openssl sha1 lynis-${VERSION}.tar.gz  | cut -f2 -d" ")
+elif which sha1sum >/dev/null; then
+    _SHA1=$(sha1sum lynis-${VERSION}.tar.gz  | cut -f1 -d" ")
+else
+    echo_exit "unable to verify download (exit)"
+fi
 
-(
-  cd lynis-${VERSION}
-  ./lynis show version ; ./lynis update info
-  echo "Lynis is ready to use. Please find it here: $(pwd)"
-)
+if [ "$_SHA1" == "$SHA1_HASH" ]; then
+    echo "extracting lynis and show version and update info ..."
+    tar xfz lynis-${VERSION}.tar.gz
+    (
+        cd lynis-${VERSION}
+        ./lynis show version ; ./lynis update info
+        echo "Lynis is ready to use. Please find it here: $(pwd)"
+    )
+else 
+    echo_exit "download file integrity could not be verified! (exit)"
+fi
